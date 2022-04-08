8d623b5e82581fd1df32a1822158f079e18d3bcd
Najla Parker <najla08@gmail.com>
2022-04-08 11:00:18 -0600

    Adding Recipe User models

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express()
+const Recipe = require('./models/Recipe')
+const User = require('./models/User')
 
 
 const PORT = process.env.PORT || 4444;
